2df23e3c52bc72b157845aed158162b6e8a685a9
David Doyle <david@lumidion.com>
2022-01-22 20:05:39 +0100

    staging tests

--- src/test/utils/Parser.test.ts
+++ src/test/utils/Parser.test.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import { parseCarBody } from "../../main/utils/Parser"
 
-// import { carIdParserMiddelware } from "../../main/utils/Parser"
 describe("Parser", () => {
   describe("Car body parser", () => {
     let mockRequest: Partial<Request>
@@ -11,6 +10,7 @@ describe("Parser", () => {
     beforeEach(() => {
       mockRequest = {}
       mockResponse = {
+        status: jest.fn(),
         json: jest.fn(),
       }
     })
@@ -21,23 +21,24 @@ describe("Parser", () => {
         mockResponse as Response,
         nextFunction
       )
-      expect(mockResponse.statusCode).toBe(400)
+
+      expect(mockResponse.status).toBeCalledWith(400)
     })
-    it("returns 200 when parsing a body with all required fields", async () => {
-      mockRequest = {
-        body: { brandId: 1, transmission: "automatic" },
-      }
-      parseCarBody(
-        mockRequest as Request,
-        mockResponse as Response,
-        nextFunction
-      )
-      expect(mockResponse.statusCode).toBe(400)
-    })
+    // it("returns 200 when parsing a body with all required fields", async () => {
+    //   mockRequest = {
+    //     body: { brandId: 1, transmission: "automatic" },
+    //   }
+    //   parseCarBody(
+    //     mockRequest as Request,
+    //     mockResponse as Response,
+    //     nextFunction
+    //   )
+    //   expect(mockResponse.statusCode).toBe(400)
+    // })
   })
-  it("parses valid input correctly", () => {
-    const input = "1d18fb0a-9a01-499c-a9ba-5a704c1240e6" as unknown
-    const expectedOutput = "1d18fb0a-9a01-499c-a9ba-5a704c1240e6"
-    // expect(parseCarIdInput(input)).toBe(expectedOutput)
-  })
+  //   it("parses valid input correctly", () => {
+  //     const input = "1d18fb0a-9a01-499c-a9ba-5a704c1240e6" as unknown
+  //     const expectedOutput = "1d18fb0a-9a01-499c-a9ba-5a704c1240e6"
+  //     // expect(parseCarIdInput(input)).toBe(expectedOutput)
+  //   })
 })
